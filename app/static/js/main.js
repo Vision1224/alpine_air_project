57b9cf753dc132fb1577ea2f32178ba398932b45
Vision1224 <damianv2420@gmail.com>
2023-06-12 17:20:45 -0600

    Cleaned up and took off some comments

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -3,19 +3,15 @@ document
   .addEventListener("submit", function (event) {
     event.preventDefault();
 
-    // Get form data
     const form = document.getElementById("purchaseOrderForm");
     const formData = new FormData(form);
 
-    // Send form data to the backend
     fetch("/", {
-      // Update the URL to match the root URL of the backend
       method: "POST",
       body: formData,
     })
       .then((response) => response.text())
       .then((data) => {
-        // Display response from the backend
         const responseDiv = document.getElementById("response");
         responseDiv.innerHTML = data;
         responseDiv.style.display = "block";
